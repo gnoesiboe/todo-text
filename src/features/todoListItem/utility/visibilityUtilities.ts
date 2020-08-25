0afa8e8caec59ff2e4f419f28e23370bf94f2124
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2020-08-25 13:51:48 +0200

    Project formatter

--- src/features/todoListItem/utility/visibilityUtilities.ts
+++ src/features/todoListItem/utility/visibilityUtilities.ts
@@ -1,40 +1,57 @@
 import { isHeading } from './../../../model/TodoListItem';
 import type { TodoListItem } from '../../../model/TodoListItem';
 
-const applyStrikeThrough = (value: string) => {
+type Formatter = (value: string) => string;
+
+const applyStrikeThrough: Formatter = (value) => {
     return value.replace(
         /~~([^~]+)~~/,
         '<span class="todo-list-item__value--removed">$1</span>',
     );
 };
 
-const applyLineBreaks = (value: string) => {
+const applyLineBreaks: Formatter = (value) => {
     return value.replace(/\n/g, '<br />');
 };
 
-const applyStrong = (value: string) => {
+const applyStrong: Formatter = (value) => {
     return value.replace(/\*\*([^*]+)\*\*/g, '<strong>$1</strong>');
 };
 
-const applyTagging = (value: string) => {
+const applyTagging: Formatter = (value) => {
     return value.replace(
         /(\@[^\s]+)/g,
         '<i class="todo-list-item__value__tag">$1</i>',
     );
 };
 
-const applyLinks = (value: string) => {
+const applyLinks: Formatter = (value) => {
     return value.replace(
         /\[([^\]]+)\]\(([^)]+)\)/g,
         '<a href="$2" class="todo-list-item__value__link" target="_blank" rel="noreferrer">$1</a>',
     );
 };
 
-const formatAsHeading = (value: string) => {
+const applyProjects: Formatter = (value) => {
+    return value.replace(
+        /(\[[^\]]+\])/g,
+        '<span class="todo-list-item__value__project">$1</span>',
+    );
+};
+
+const formatAsHeading: Formatter = (value) => {
     return value.replace(
         /^# (.+)$/g,
         '<span class="todo-list-item__value__heading">$1</span>',
     );
+};
+
+const applyFormatters = (value: string, ...formatters: Formatter[]): string => {
+    let out: string = value;
+
+    formatters.forEach((applyTo) => (out = applyTo(out)));
+
+    return out;
 };
 
 export const prepareForVisibility = (item: TodoListItem): string => {
@@ -42,9 +59,13 @@ export const prepareForVisibility = (item: TodoListItem): string => {
         return formatAsHeading(item.value);
     }
 
-    return applyTagging(
-        applyLinks(
-            applyStrong(applyStrikeThrough(applyLineBreaks(item.value))),
-        ),
+    return applyFormatters(
+        item.value,
+        applyTagging,
+        applyLinks,
+        applyProjects,
+        applyStrong,
+        applyStrikeThrough,
+        applyLineBreaks,
     );
 };
