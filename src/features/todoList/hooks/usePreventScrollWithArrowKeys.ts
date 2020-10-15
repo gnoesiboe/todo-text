591c1ce60cf73cd4943aa41cf68894d80d1ddfb4
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2020-10-15 17:56:21 +0200

    Don't block arrow navigation when editing

--- src/features/todoList/hooks/usePreventScrollWithArrowKeys.ts
+++ src/features/todoList/hooks/usePreventScrollWithArrowKeys.ts
@@ -1,7 +1,14 @@
+import { useTodoContext } from './../../../context/todoContext/TodoContext';
 import { useEffect } from 'react';
 
 export default function usePreventScrollWithArrowKeys() {
+    const { isEditing } = useTodoContext();
+
     useEffect(() => {
+        if (isEditing) {
+            return;
+        }
+
         const onKeyDown = (event: WindowEventMap['keydown']) => {
             if (event.key === 'ArrowDown' || event.key === 'ArrowUp') {
                 event.preventDefault();
