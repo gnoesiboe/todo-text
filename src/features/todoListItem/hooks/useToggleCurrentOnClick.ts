0fee3e11f4feaf48c3491f0b2a513f06000e6482
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2020-12-16 18:08:00 +0100

    Fixed bug where bubbling of event would cause the container to register the click event also, and deselect it

--- src/features/todoListItem/hooks/useToggleCurrentOnClick.ts
+++ src/features/todoListItem/hooks/useToggleCurrentOnClick.ts
@@ -1,13 +1,18 @@
+import { MouseEventHandler } from 'react';
 import { TodoListItem } from 'model/TodoListItem';
 import { useTodoContext } from 'context/todoContext/TodoContext';
 
 export default function useToggleCurrentOnClick(item: TodoListItem) {
     const { toggleCurrentItem, isEditing } = useTodoContext();
 
-    const onClick = () => {
+    const onClick: MouseEventHandler = (event) => {
         if (isEditing) {
             return;
         }
+
+        // Prevent event from bubbling up to MainContainer and register a click there
+        // that will deselect it again.
+        event.stopPropagation();
 
         toggleCurrentItem(item.id);
     };
