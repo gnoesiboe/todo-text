b01b510891bf1bd432047a75073a66fa6d0ee2e9
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2020-12-17 08:55:50 +0100

    Quickfix to get adding todos for mobile, to work again

--- src/features/addTodo/hooks/useAddTodoOnButtonClick.ts
+++ src/features/addTodo/hooks/useAddTodoOnButtonClick.ts
@@ -4,10 +4,14 @@ import { MouseEventHandler } from 'react';
 export default function useAddTodoOnButtonClick() {
     const { createNewItemAtTheStart, isEditing } = useTodoContext();
 
-    const onClick: MouseEventHandler = () => {
+    const onClick: MouseEventHandler = (event) => {
         if (isEditing) {
             return;
         }
+
+        // prevent click event from being caught by the MainContainer, wich
+        // results in the current todo list item being lost.
+        event.stopPropagation();
 
         createNewItemAtTheStart();
     };
