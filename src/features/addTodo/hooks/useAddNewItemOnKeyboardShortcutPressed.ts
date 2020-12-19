a6b056bcbd5b00c09a325e953f3c4bc21aa4272b
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2020-12-19 16:08:58 +0100

    Fixed bug where, when typing 'a' in notes, a new todo would be added

--- src/features/addTodo/hooks/useAddNewItemOnKeyboardShortcutPressed.ts
+++ src/features/addTodo/hooks/useAddNewItemOnKeyboardShortcutPressed.ts
@@ -1,3 +1,4 @@
+import { useIsEditingNotes } from 'context/notesContext/NotesContext';
 import { checkKeyDefinitionIsPressed } from 'utility/keyboardNavigationUtilities';
 import { useEffect } from 'react';
 import { useTodoContext } from 'context/todoContext/TodoContext';
@@ -5,7 +6,6 @@ import {
     createNewItemAfterCurrent as createNewItemAfterCurrentKeyDefinition,
     createNewItemBeforeCurrent as createNewItemBeforeCurrentKeyDefinition,
 } from 'constants/keyDefnitions';
-import useManageIsEditingState from 'context/notesContext/hooks/useManageIsEditingState';
 
 export default function useAddNewItemOnKeyboardShortcutPressed() {
     const {
@@ -17,7 +17,7 @@ export default function useAddNewItemOnKeyboardShortcutPressed() {
         startEdit,
     } = useTodoContext();
 
-    const { isEditing: isEditingNotes } = useManageIsEditingState();
+    const isEditingNotes = useIsEditingNotes();
 
     useEffect(() => {
         if (isEditing || isEditingNotes) {
