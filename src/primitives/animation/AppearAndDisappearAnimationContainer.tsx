4db0e6831034acd789a0f29f5a6cb2096fa2701e
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2021-05-22 13:47:29 +0200

    Use linear animation

--- src/primitives/animation/AppearAndDisappearAnimationContainer.tsx
+++ src/primitives/animation/AppearAndDisappearAnimationContainer.tsx
@@ -11,10 +11,12 @@ const AppearAndDisappearAnimationContainer: React.FC<Props> = ({
     children,
 }) => (
     <motion.div
-        layout
         initial={{ opacity: 0, x: xAnimation }}
         animate={{ opacity: 1, x: 0 }}
         exit={{ opacity: 0, x: -xAnimation }}
+        transition={{
+            type: 'linear',
+        }}
     >
         {children}
     </motion.div>
