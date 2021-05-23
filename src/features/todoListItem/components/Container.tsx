651defe0014ee0d81b482e0d4749c49442de35e1
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2021-05-23 13:14:15 +0200

    Transition background color for current todo

--- src/features/todoListItem/components/Container.tsx
+++ src/features/todoListItem/components/Container.tsx
@@ -14,7 +14,9 @@ const Container = styled(motion.div)<{
         ${({ current, isSorting }) => (current && !isSorting ? 30 : 5)}px -10px;
     padding: 5px 0 3px 0;
     border-radius: 5px;
-    transition: max-height 1s ease-in;
+    transition: max-height 1s ease-in, background-position 0.3s ease-out;
+    background-size: 200% 100%;
+    background-image: linear-gradient(to right, #fff 50%, #eee 50%);
 
     ${({ isEditing, current }) => {
         if (!current) {
@@ -23,7 +25,7 @@ const Container = styled(motion.div)<{
 
         return isEditing
             ? 'background: rgba(231, 111, 81, 0.3) !important;'
-            : 'background: #eee;';
+            : `background-position: -100% 0;`;
     }}
 
     ${({ isDragging, theme }) =>
