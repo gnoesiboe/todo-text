6d1eeb2198a2af968f6bf1e01fc2ae62bcda221c
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2021-05-23 22:29:03 +0200

    Better current todo list item transition

--- src/features/todoListItem/components/Container.tsx
+++ src/features/todoListItem/components/Container.tsx
@@ -2,6 +2,9 @@ import styled from 'styled-components';
 import { ParsedTodoValue, TodoListItem } from 'model/TodoListItem';
 import { motion } from 'framer-motion';
 
+const compensationMargin = 10;
+const currentBorderWidth = 12;
+
 const Container = styled(motion.div)<{
     item: TodoListItem<ParsedTodoValue | string>;
     current: boolean;
@@ -11,12 +14,11 @@ const Container = styled(motion.div)<{
 }>`
     position: relative;
     margin: 0 0
-        ${({ current, isSorting }) => (current && !isSorting ? 30 : 5)}px -10px;
+        ${({ current, isSorting }) => (current && !isSorting ? 30 : 5)}px -${compensationMargin}px;
     padding: 5px 0 3px 0;
-    border-radius: 5px;
-    transition: max-height 1s ease-in, background-position 0.3s ease-out;
-    background-size: 200% 100%;
-    background-image: linear-gradient(to right, #fff 50%, #eee 50%);
+    border-radius: 3px;
+    transition: background-color 0.3s;
+    background: #fff;
 
     ${({ isEditing, current }) => {
         if (!current) {
@@ -24,8 +26,12 @@ const Container = styled(motion.div)<{
         }
 
         return isEditing
-            ? 'background: rgba(231, 111, 81, 0.3) !important;'
-            : `background-position: -100% 0;`;
+            ? 'background-color: rgba(231, 111, 81, 0.3) !important;'
+            : `
+                background-color: #f3f3f3;
+                border-left: ${currentBorderWidth}px solid #ccc;
+                margin-left: -${currentBorderWidth + compensationMargin}px;
+            `;
     }}
 
     ${({ isDragging, theme }) =>
--- src/primitives/animation/AppearAndDisappearAnimationContainer.tsx
+++ src/primitives/animation/AppearAndDisappearAnimationContainer.tsx
@@ -11,11 +11,12 @@ const AppearAndDisappearAnimationContainer: React.FC<Props> = ({
     children,
 }) => (
     <motion.div
-        initial={{ opacity: 0, x: xAnimation }}
+        initial={{ opacity: 0, x: -xAnimation }}
         animate={{ opacity: 1, x: 0 }}
-        exit={{ opacity: 0, x: -xAnimation }}
+        exit={{ opacity: 0, x: xAnimation }}
         transition={{
             type: 'linear',
+            delay: 0.3,
         }}
     >
         {children}
